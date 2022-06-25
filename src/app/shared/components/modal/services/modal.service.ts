be655669cc083341c130251d7192b9fdedf7668a
RyamSousa <ryam.sousa123@gmail.com>
2022-06-25 10:27:53 -0300

    :sparkles: creation of the component ref in service

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,20 +1,39 @@
-import { Injectable, TemplateRef } from "@angular/core";
-
-export interface ModalConfig {
-	templateRef: TemplateRef<any>;
-	title: string;
-}
+import {
+	ComponentFactory,
+	ComponentFactoryResolver,
+	ComponentRef,
+	Injectable,
+	Injector,
+} from "@angular/core";
+import { ModalConfig } from "../interfaces/modal-config";
+import { ModalComponent } from "../modal.component";
 
 @Injectable()
 export class ModalService {
+	private componentFactory: ComponentFactory<ModalComponent>;
+	constructor(
+		private componentFactoryResolver: ComponentFactoryResolver,
+		private injector: Injector
+	) {
+		this.componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+			ModalComponent
+		);
+	}
+
 	public open(config: ModalConfig): ModalRef {
-		console.log("open called");
-		return new ModalRef();
+		const componentRef = this.createComponentRef();
+		componentRef.instance.config = config;
+		return new ModalRef(componentRef);
+	}
+
+	private createComponentRef(): ComponentRef<ModalComponent> {
+		return this.componentFactory.create(this.injector);
 	}
 }
 
 export class ModalRef {
+	constructor(private componentRef: ComponentRef<ModalComponent>) {}
 	public close(): void {
-		console.log("close called");
+		this.componentRef.destroy();
 	}
 }
