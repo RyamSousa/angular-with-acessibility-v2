3291aa9fce03c7d46ac6db1478d8f367836a130b
RyamSousa <ryam.sousa123@gmail.com>
2022-06-25 09:51:03 -0300

    :hammer: :sparkles: refact indentation and implement form modal in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from "@angular/core";
+import { ModalRef, ModalService } from "./shared/components/modal/services/modal.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'a11y-p2';
+	@ViewChild("modal") public modalTemplateRef: TemplateRef<any>;
+	public firstName = "Ryam";
+	public modalRef: ModalRef;
+	title = "a11y-p2";
+
+	constructor(private modalService: ModalService) {}
+
+	public show() {
+		this.modalRef = this.modalService.open({
+			templateRef: this.modalTemplateRef,
+			title: "User Details",
+		});
+	}
 }
