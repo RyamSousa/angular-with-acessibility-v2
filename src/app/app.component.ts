3040e22d817c8b9b0cef29917fa3568c906409ea
RyamSousa <ryam.sousa123@gmail.com>
2022-06-27 21:04:46 -0300

    reuse 'fade' animation at the 'app' component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, TemplateRef, ViewChild } from "@angular/core";
+import { fade } from "./shared/animations/fade";
 import { ModalRef, ModalService } from "./shared/components/modal/services/modal.service";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"],
+	animations: [fade],
 })
 export class AppComponent {
 	@ViewChild("modal") public modalTemplateRef: TemplateRef<any>;
 	public firstName = "Ryam";
 	public modalRef: ModalRef;
+	public info = false;
 	title = "a11y-p2";
 
 	constructor(private modalService: ModalService) {}
